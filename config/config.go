0a2eb05555be0e655ac992b0a253b51873af3aaf
sqars <sqars89@gmail.com>
2018-11-09 19:05:47 +0100

    create config file on start

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
 )
@@ -12,6 +13,14 @@ type Config struct {
 	CSVFilePattern  string
 }
 
+// newDefault creates new Config instance with default values
+func newDefault() Config {
+	return Config{
+		JSONFilePattern: "/*i18n*",
+		CSVFilePattern:  "/*.csv",
+	}
+}
+
 // GetConfig reads config file and returns Config struct
 func GetConfig() (Config, error) {
 	conf := Config{}
@@ -19,17 +28,30 @@ func GetConfig() (Config, error) {
 	if err != nil {
 		return conf, err
 	}
-	f, err := os.Open(wd + "/config.json")
+	path := wd + "/config.json"
+	f, err := os.Open(path)
 	if err != nil {
-		return conf, err
+		fmt.Println("Cannot find config file. Creating one.")
+		f, err := os.Create(path)
+		if err != nil {
+			return conf, err
+		}
+		conf = newDefault()
+		bytes, err := json.MarshalIndent(conf, "", "\t")
+		if err != nil {
+			return conf, err
+		}
+		f.Write(bytes)
+	} else {
+		bytes, err := ioutil.ReadAll(f)
+		if err != nil {
+			return conf, err
+		}
+		err = json.Unmarshal(bytes, &conf)
+		if err != nil {
+			return conf, err
+		}
 	}
-	bytes, err := ioutil.ReadAll(f)
-	if err != nil {
-		return conf, err
-	}
-	err = json.Unmarshal(bytes, &conf)
-	if err != nil {
-		return conf, err
-	}
+	f.Close()
 	return conf, nil
 }
